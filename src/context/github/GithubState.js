05475227d0d834176dc2284cbad682503546a4dc
drewatienza <atienza.drew@gmail.com>
2020-12-15 09:57:37 -0800

    created reducers and actions

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -21,7 +21,16 @@ const GithubState = props => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // Search Users
+  const searchUsers = async text => {
+    setLoading();
 
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  }
 
   // Get User
 
@@ -33,7 +42,7 @@ const GithubState = props => {
 
 
   // Set Loading
-
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider 
@@ -41,7 +50,8 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
